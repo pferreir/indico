969f2ac8e5366b92d980b253a8f922c570e4cf53
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-02-19 11:13:45 +0100

    fixup! Use webpack-merge, plugin handling commands

--- webpack/base.js
+++ webpack/base.js
@@ -40,7 +40,8 @@ function _resolveTheme(rootPath, indicoClientPath, filePath) {
 
 export function getThemeEntryPoints(config, prefix) {
     const themes = config.themes;
-    const indicoClientPath = path.join(config.indico.build.clientPath, 'styles');
+    const indicoClientPath = path.join(config.isPlugin ? config.indico.build.clientPath :
+                                       config.build.clientPath, 'styles');
     const rootPath = path.join(config.build.rootPath);
 
     return Object.assign(...Object.keys(themes).map((k) => {
